1f9100edb94cb088f577150438d8fef49e7ff222
Cody Karigaca <ckarigac@ucsc.edu>
2025-04-19 12:29:35 -0700

    some changes

---
+++ experiment3/js/p2_solution.js
@@ -0,0 +1,225 @@
+/* exported generateGrid, drawGrid */
+/* global placeTile */
+
+
+
+function generateRiver(grid, numRows, numCols) {
+    let startRow = random(0, numRows) | 0;
+    let startCol = random(0, numCols) | 0;
+    let riverLength = random(100, 400);
+    let riverWidth = random(3, 7);
+
+    let row = startRow;
+    let col = startCol;
+
+    let direction = random(4) | 0;
+    for (let i = 0; i < riverLength; i++) {
+        // Mark the current cell as part of the river
+        grid[row][col] = ".";
+
+        //For the width of the river increase river sized based on direction
+        for (let j = 0; j < riverWidth; j++) {
+            if (row + j < grid.length - 1) {
+                grid[row + j][col] = ".";
+            }
+        }
+
+        //Random Chancto change direction
+        if (random() > 0.5) {
+            direction = random(4) | 0;
+        }
+        if (random() > 0.5) {
+            riverWidth = random(3, 7);
+        }
+
+        if (direction === 0 && row > 0) {
+            row--; //North
+        } else if (direction === 1 && row < grid.length - 1) {
+            row++; //South
+        } else if (direction === 2 && col < grid[0].length - 1) {
+            col++; //East
+        } else if (direction === 3 && col > 0) {
+            col--; //West
+        }
+    }
+}
+
+function generateForest(grid, numRows, numCols) {
+    let centerRow = random(0, numRows) | 0;
+    let centerCol = random(0, numCols) | 0;
+    let radius = random(5, 12);
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            // Calculate distance from the center
+            let distance = Math.sqrt((i - centerRow) ** 2 + (j - centerCol) ** 2);
+            distance += random(1, 5);
+            if (distance <= radius) {
+                grid[i][j] = "T";
+            }
+        }
+    }
+}
+
+function generateGrid(numCols, numRows) {
+    let grid = [];
+
+    //Rectangluar room bounds
+    //let dungeonX=5
+    for (let i = 0; i < numRows; i++) {
+        let row = [];
+        for (let j = 0; j < numCols; j++) {
+            row.push("_");
+        }
+        grid.push(row);
+    }
+
+    for (let i = 0; i < random(1, 4); i++) {
+        generateForest(grid, numRows, numCols);
+    }
+    for (let i = 0; i < random(1, 2); i++) {
+        generateRiver(grid, numRows, numCols);
+    }
+
+    return grid;
+}
+
+function drawGrid(grid) {
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            // Check if the current cell is a river or forest tile
+            // and draw the appropriate tile
+            if (gridCheck(grid, i, j, "_")) {
+                placeTile(i, j, random(4) | 0, 0);
+            } else if (gridCheck(grid, i, j, "T")) {
+                placeTile(i, j, random(4) | 0, 0);
+                placeTile(i, j, 14, 0);
+            } else if (gridCheck(grid, i, j, ".")) {
+                drawContext(grid, i, j, ".", 0, 0);
+            }
+        }
+    }
+}
+
+const lookup = [
+    [[0, 0]], //0
+    [
+        [4, 1],
+        [5, 2],
+        [6, 1],
+    ], //1
+    [
+        [5, 2],
+        [6, 1],
+        [5, 0],
+    ], //2 done
+    [
+        [5, 2],
+        [6, 1],
+    ], //3 done
+    [
+        [10, 2],
+        [5, 0],
+        [4, 1],
+    ], //4 done
+    [[9, 2]], //5 (doneish)
+    [
+        [10, 2],
+        [5, 0],
+    ], //6 done
+    [[10, 2]], //7 done
+    [
+        [9, 1],
+        [11, 1],
+        [5, 0],
+    ], //8 done(ish)
+    [
+        [4, 1],
+        [6, 1],
+    ], //9
+    [[11, 0]], //10 done
+    [[11, 1]], //11 done
+    [[9, 0]], //12 done
+    [[9, 1]], //13 done
+    [[10, 0]], //14 done
+    [[10, 1]], //15 done
+];
+
+function gridCheck(grid, i, j, target) {
+    // Check if i, j is within the grid boundaries
+    if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length) {
+        return grid[i][j] === target; // Return true if grid[i][j] matches target
+    }
+    return false; // Out of bounds or no match
+}
+
+function gridCode(grid, i, j, target) {
+    // Form a 4-bit code using gridCheck on the north/south/east/west
+    //neighbors of i,j for the target code.
+    //You might us an example like (northBit<<0)
+    // +(southBit<<1)+(eastBit<<2)+(westBit<<3).
+    //First check if there is a tile using gridCheck
+
+    let code = 0;
+    //North
+    if (gridCheck(grid, i - 1, j, target)) {
+        code += 1;
+    }
+    //West
+    if (gridCheck(grid, i, j - 1, target)) {
+        code += 2;
+    }
+    //East
+    if (gridCheck(grid, i, j + 1, target)) {
+        code += 4;
+    }
+    //South
+    if (gridCheck(grid, i + 1, j, target)) {
+        code += 8;
+    }
+    return code;
+}
+
+function drawContext(grid, i, j, target, ti, tj) {
+    //Get the code for this location and target.
+    //Use the code as an array index to get a pair of tile offset numbers.
+    //const [tiOffset, tjOffset] = lookup[code]; placeTile(i, j, ti + tiOffset, tj + tjOffset);
+    let binary = gridCode(grid, i, j, target);
+    //let code = lookup[binary];
+
+    //placeTile(i, j, code[0],code[1]);
+    //const [tiOffset, tjOffset] = lookup[binary];
+    let nL = random(0, 1);
+    let nH = random(1, 2);
+    let randomTile = random([1, 2, 3])
+    let n = noise(nL, nH, frameCount * random(0.001, 0.005));
+    if (n > 0.6) {
+
+        //place special water
+        /*
+        if (randomTile == 1) {
+            placeTile(i, j, 1, 13);
+        }else if (randomTile == 2) {
+            placeTile(i, j, 2, 13);
+        } else if (randomTile == 3) {
+            placeTile(i, j, 3, 13);
+        }
+        */
+        placeTile(i, j, 2, 13);
+
+    } else {
+        //Place normal water tile
+        placeTile(i, j, 0, 13);
+    }
+
+
+    for (let x = 0; x < lookup[binary].length; x++) {
+        let t = lookup[binary][x];
+        placeTile(i, j, t[0], t[1]);
+    }
+
+    
+}
+
+
--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -6,24 +6,60 @@
 // Note that p5.js looks for a file called sketch.js
 
 // Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+// In a longer project I like to put these in a separate fi
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+/* exported preload, setup, draw, placeTile */
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+/* global generateGrid drawGrid */
+
+let seed = 0;
+let tilesetImage;
+let currentGrid = [];
+let numRows, numCols;
+
+function preload() {
+  tilesetImage = loadImage(
+    "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
+  );
+}
+
+function reseed() {
+  seed = (seed | 0) + 1109;
+  randomSeed(seed);
+  noiseSeed(seed);
+  select("#seedReport").html("seed " + seed);
+  regenerateGrid();
+}
+
+function regenerateGrid() {
+  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
+  reparseGrid();
+}
+
+function reparseGrid() {
+  currentGrid = stringToGrid(select("#asciiBox").value());
+}
+
+function gridToString(grid) {
+  let rows = [];
+  for (let i = 0; i < grid.length; i++) {
+    rows.push(grid[i].join(""));
+  }
+  return rows.join("\n");
+}
+
+function stringToGrid(str) {
+  let grid = [];
+  let lines = str.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let row = [];
+    let chars = lines[i].split("");
+    for (let j = 0; j < chars.length; j++) {
+      row.push(chars[j]);
     }
-
-    myMethod() {
-        // code to run when method is called
-    }
+    grid.push(row);
+  }
+  return grid;
 }
 
 function resizeScreen() {
@@ -37,43 +73,37 @@ function resizeScreen() {
 // setup() function is called once when the program starts
 function setup() {
   // place our canvas, making it fit our container
+  numCols = select("#asciiBox").attribute("rows") | 0;
+  numRows = select("#asciiBox").attribute("cols") | 0;
+
   canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  let canvas = createCanvas(16 * numCols, 16 * numRows);
   canvas.parent("canvas-container");
+
+  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+
+  select("#reseedButton").mousePressed(reseed);
+  select("#asciiBox").input(reparseGrid);
+
+  reseed();
+
   // resize canvas is the page is resized
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
+  $(window).resize(function () {
     resizeScreen();
   });
   resizeScreen();
+
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  randomSeed(seed);
+  drawGrid(currentGrid);
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function placeTile(i, j, ti, tj) {
+  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
 }
